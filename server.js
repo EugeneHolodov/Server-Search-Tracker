d0f3b4ff2a0b02ea5a9deb091d15e2442de5d5ea
Yevhenii Holodov <jekassansai720@gmail.com>
2024-02-12 21:03:24 +0100

    some

--- server.js
+++ server.js
@@ -35,12 +35,6 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://client-phi-sooty.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 app.use("/uploads", express.static("uploads"));
 
 app.get('/', (req, res) => {
