79c519a764b9baccbe8e2cc256489f60b847cc95
Yevhenii Holodov <jekassansai720@gmail.com>
2024-02-12 20:21:18 +0100

    some commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ import {
 
 const connectDB = async () => {
   try {
-      await mongoose.connect(process.env.MONGODB_CONNECT_URI)
+      await mongoose.connect(process.env.MONGODB_URI)
       console.log("Connect to MongoDB successfully")
   } catch (error) {
       console.log("Connect failed " + error.message )
